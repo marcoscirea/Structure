7d31fd29895838b9e83b6a1954ae62673d073d8e
marcoscirea <marco.prolog@gmail.com>
2014-05-28 15:53:03 +0200

    Added FrontParallax and disabling the cameras for parallax when useless, also better structured parallax layers and tags

---
+++ Assets/Scripts/Camera/ParallaxDisable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxDisable : MonoBehaviour {
+
+    void Awake(){
+        if (GameObject.FindGameObjectsWithTag("ParallaxFront").Length==0)
+        {
+            Debug.Log("No ParallaxFront objects, disabling relative camera");
+            transform.FindChild("ParallaxFrontCamera").gameObject.SetActive(false);
+        }
+        if (GameObject.FindGameObjectsWithTag("ParallaxBack").Length==0)
+        {
+            Debug.Log("No ParallaxFront objects, disabling relative camera");
+            transform.FindChild("ParallaxBackCamera").gameObject.SetActive(false);
+        }
+    }
+}
