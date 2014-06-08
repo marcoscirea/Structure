9545952d7d5a5bf12f92f50fb0389cf26530140b
marcoscirea <marco.prolog@gmail.com>
2014-06-08 13:02:18 +0200

    Very basic save system

---
+++ Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using PixelCrushers.DialogueSystem;
+
+public class SaveLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyUp(KeyCode.S))
+        {
+            GetComponent<GameSaver>().SaveGame();
+        }
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            GetComponent<GameSaver>().LoadGame();
+        }
+	}
+}
